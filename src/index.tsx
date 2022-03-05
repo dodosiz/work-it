a733299bef49c4ef2da925b10e5d302c17e9a555
Theodosis Asvestopoulos <t.v.asvestopoulos@gmail.com>
2022-03-05 18:28:55 +0100

    Configured Redux Tookit and organised files.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./app";
+import { App } from "./view/app";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./data/store";
 
 ReactDOM.render(
-	<App />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.getElementById("app"),
 );
