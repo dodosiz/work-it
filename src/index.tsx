4273fa4d7a738ca5b2e4da1b50cf647eb7d68c4e
Theodosis Asvestopoulos <t.v.asvestopoulos@gmail.com>
2022-03-09 20:25:46 +0100

    Removed the App component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./view/app";
 import { Provider } from "react-redux";
 import { store } from "./data/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
 	</Provider>,
 	document.getElementById("app"),
 );
--- src/view/app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Button } from "react-bootstrap";
-
-export function App() {
-	return <div className="container">
-		<h1>Work It</h1>
-		<Button variant="outline-primary">Bootstrap loaded</Button>
-	</div>;
-}
