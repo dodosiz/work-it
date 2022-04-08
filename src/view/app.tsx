0cf595b1de6c50d979611b995574a5abf7d9bbe1
Theodosis Asvestopoulos <t.v.asvestopoulos@gmail.com>
2022-04-08 22:50:18 +0200

    Moved the user modal to the user page

--- src/view/app.tsx
+++ src/view/app.tsx
@@ -1,50 +1,31 @@
 import * as React from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import {
-	closeUserForm,
-	openUserForm,
-	userFormOpenedSelector,
-} from "../data/users";
-import { ModalDialog } from "./modal-dialog";
-import { CreateUserForm } from "./users/create-user-form";
+import { useDispatch } from "react-redux";
+import { openUserForm } from "../data/users";
 import { UsersPage } from "./users/users-page";
 
 export function App() {
 	const dispatch = useDispatch();
-	const userFormOpened = useSelector(userFormOpenedSelector);
 	const handleCreateUserClick = () => {
 		dispatch(openUserForm());
 	};
-	const handleCloseUserForm = () => {
-		dispatch(closeUserForm());
-	};
 	return (
-		<>
-			<ModalDialog
-				handleClose={handleCloseUserForm}
-				show={userFormOpened}
-				title="Create new user"
-			>
-				<CreateUserForm />
-			</ModalDialog>
-			<div>
-				<Navbar expand="lg" variant="light" bg="light">
-					<Container>
-						<Navbar.Brand>Work IT</Navbar.Brand>
-						<Navbar.Collapse id="basic-navbar-nav">
-							<Nav className="me-auto">
-								<Button variant="success" onClick={handleCreateUserClick}>
-									Create User
-								</Button>
-							</Nav>
-						</Navbar.Collapse>
-					</Container>
-				</Navbar>
-				<div className="container main">
-					<UsersPage />
-				</div>
+		<div>
+			<Navbar expand="lg" variant="light" bg="light">
+				<Container>
+					<Navbar.Brand>Work IT</Navbar.Brand>
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Button variant="success" onClick={handleCreateUserClick}>
+								Create User
+							</Button>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<div className="container main">
+				<UsersPage />
 			</div>
-		</>
+		</div>
 	);
 }
--- src/view/users/users-page.tsx
+++ src/view/users/users-page.tsx
@@ -1,9 +1,25 @@
 import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { closeUserForm, userFormOpenedSelector } from "../../data/users";
+import { ModalDialog } from "../modal-dialog";
+import { CreateUserForm } from "./create-user-form";
 import { UsersList } from "./users-list";
 
 export function UsersPage() {
+	const dispatch = useDispatch();
+	const userFormOpened = useSelector(userFormOpenedSelector);
+	const handleCloseUserForm = () => {
+		dispatch(closeUserForm());
+	};
 	return (
 		<div>
+			<ModalDialog
+				handleClose={handleCloseUserForm}
+				show={userFormOpened}
+				title="Create new user"
+			>
+				<CreateUserForm />
+			</ModalDialog>
 			<UsersList />
 		</div>
 	);
